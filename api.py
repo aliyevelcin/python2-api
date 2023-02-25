5faff68d1b69fffc0fbe4daaa286007f8dd16ccd
aliyevelcin <elcinaliyev706@gmail.com>
2023-02-26 01:09:00 +0400

    first commit

---
+++ api.py
@@ -0,0 +1,9 @@
+import requests
+ 
+
+# salam = input()
+# pre_result = requests.get(f'https://dog.ceo/api/breed/{salam}/images/random')
+# result = pre_result.json()
+# print(f'adi: {result["message"]}')
+# print(f'adi: {result["status"]}')
+
