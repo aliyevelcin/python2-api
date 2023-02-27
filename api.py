aae772fed6f93864c29d3f5b845fb8c3ea623144
aliyevelcin <elcinaliyev706@gmail.com>
2023-02-27 14:58:46 +0400

    m

--- api.py
+++ api.py
@@ -7,3 +7,19 @@ import requests
 # print(f'adi: {result["message"]}')
 # print(f'adi: {result["status"]}')
 
+
+
+
+import requests
+
+pre_result = requests.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m')
+
+result = pre_result.json()
+
+print(f'Temperatur: {result["current_weather"]["temperature"]}')
+print(f'Kulek sureti: {result["current_weather"]["windspeed"]}')
+print(f'Kulek istiqameti: {result["current_weather"]["winddirection"]}')
+print(f'Saat: {result["current_weather"]["time"]}')
+ 
+
+
